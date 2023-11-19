2f3c86106f115b18c27783984802e2db9dee8d1c
Matthew Reda <m.reda94@gmail.com>
2023-11-19 13:41:25 -0500

    Updated Base Model

--- components/models/basemodel.py
+++ components/models/basemodel.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
 from typing import Optional
+
 
 class BaseModel:
   def __init__(self, name: str):
@@ -13,11 +15,19 @@ class BaseModel:
   def fit(self, X: pd.DataFrame|np.ndarray, y: Optional[pd.Series|np.ndarray]=None):
     pass
   
-  def predict(self, X: pd.DataFrame|np.ndarray):
+  def predict(self, X: pd.DataFrame|np.ndarray) -> pd.Series|np.ndarray:
     pass
   
   def summary(self):
     if self.fitted_model is None:
       raise ValueError('Model has not been fitted yet')
     
+  def plot_avm(self, X: pd.DataFrame|np.ndarray, *args) -> mpl.figure.Figure:
+    pass
+  
+  def plot_resid(self, X: pd.DataFrame|np.ndarray, *args) -> mpl.figure.Figure:
+    pass
+  
+  def metric(self) -> dict[str, float]:
+    pass
   
